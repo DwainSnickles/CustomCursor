3667d76e6159a57b4d996f5bff03a2ae9e8e2b25
Dwain Sickles <dwainsnickles@hotmail.com>
2023-08-22 07:18:57 -0500

    Add project files.

---
+++ CustomCursor/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace CustomCursor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.picDestination = new System.Windows.Forms.PictureBox();
+            this.picSource = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ckbScaleImage = new System.Windows.Forms.CheckBox();
+            this.txtScaleImage = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblDropImageOK = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picDestination)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSource)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Drop Image Here";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Drag image from here to destination";
+            // 
+            // picDestination
+            // 
+            this.picDestination.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picDestination.Location = new System.Drawing.Point(40, 229);
+            this.picDestination.Margin = new System.Windows.Forms.Padding(2);
+            this.picDestination.Name = "picDestination";
+            this.picDestination.Size = new System.Drawing.Size(263, 193);
+            this.picDestination.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picDestination.TabIndex = 11;
+            this.picDestination.TabStop = false;
+            this.picDestination.Paint += new System.Windows.Forms.PaintEventHandler(this.picDestination_Paint);
+            // 
+            // picSource
+            // 
+            this.picSource.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSource.Image = global::CustomCursor.Properties.Resources.Lion;
+            this.picSource.Location = new System.Drawing.Point(57, 60);
+            this.picSource.Margin = new System.Windows.Forms.Padding(2);
+            this.picSource.Name = "picSource";
+            this.picSource.Size = new System.Drawing.Size(129, 90);
+            this.picSource.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picSource.TabIndex = 12;
+            this.picSource.TabStop = false;
+            this.picSource.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picSource_MouseDown);
+            this.picSource.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picSource_MouseMove);
+            this.picSource.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picSource_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Drag and Drop Image Cursor";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ckbScaleImage);
+            this.groupBox1.Controls.Add(this.txtScaleImage);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(248, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(203, 90);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = " Scale Image Options";
+            // 
+            // ckbScaleImage
+            // 
+            this.ckbScaleImage.AutoSize = true;
+            this.ckbScaleImage.Location = new System.Drawing.Point(12, 64);
+            this.ckbScaleImage.Name = "ckbScaleImage";
+            this.ckbScaleImage.Size = new System.Drawing.Size(192, 17);
+            this.ckbScaleImage.TabIndex = 4;
+            this.ckbScaleImage.Text = "Scale Image Maintain Aspect Ratio";
+            this.ckbScaleImage.UseVisualStyleBackColor = true;
+            // 
+            // txtScaleImage
+            // 
+            this.txtScaleImage.Location = new System.Drawing.Point(12, 38);
+            this.txtScaleImage.Name = "txtScaleImage";
+            this.txtScaleImage.Size = new System.Drawing.Size(66, 20);
+            this.txtScaleImage.TabIndex = 3;
+            this.txtScaleImage.Text = "64";
+            this.txtScaleImage.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtScaleImage_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Set Max Size";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(257, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(325, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "label6";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(363, 388);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(325, 229);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(325, 269);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(325, 312);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "label9";
+            // 
+            // lblDropImageOK
+            // 
+            this.lblDropImageOK.AutoSize = true;
+            this.lblDropImageOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDropImageOK.Location = new System.Drawing.Point(92, 312);
+            this.lblDropImageOK.Name = "lblDropImageOK";
+            this.lblDropImageOK.Size = new System.Drawing.Size(153, 20);
+            this.lblDropImageOK.TabIndex = 22;
+            this.lblDropImageOK.Text = "OK to Drop Image";
+            this.lblDropImageOK.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 450);
+            this.Controls.Add(this.lblDropImageOK);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.picDestination);
+            this.Controls.Add(this.picSource);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picDestination)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSource)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox picDestination;
+        private System.Windows.Forms.PictureBox picSource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtScaleImage;
+        private System.Windows.Forms.CheckBox ckbScaleImage;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblDropImageOK;
+    }
+}
+
---
+++ CustomCursor/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+//Visit http://sourcecodedepot.com for more progamming needs
+
+namespace CustomCursor
+{
+    public partial class Form1 : Form
+    {
+        public PictureBox selected { get; private set; }
+        public bool DragDropOK { get; private set; }
+
+        int ImageHeight; int ImageWidth;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Set image for cursor or use defaultcursor
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="UseCustomImage"></param>
+        void DragImage(Image image, Boolean UseCustomImage)
+        {
+           
+            //Create a new Image 
+            Bitmap bmp = new Bitmap(image.Width, image.Height);
+
+            //draw the source image to new image
+            picSource.DrawToBitmap(bmp, new Rectangle(Point.Empty, bmp.Size));
+
+            if (ckbScaleImage.Checked)
+            {
+                bmp = (Bitmap)ResizeImage(bmp, new Size(Convert.ToInt32(txtScaleImage.Text), Convert.ToInt32(txtScaleImage.Text)));
+
+            }
+
+            //in order to check if drag drop ok have to include width and height
+            ImageWidth = bmp.Width;
+            ImageHeight = bmp.Height;
+
+            //optionally define a transparent color
+            //bmp.MakeTransparent(Color.White);
+
+            //create new cursor to drag
+            Cursor cur = new Cursor(bmp.GetHicon());
+            Cursor.Current = cur;
+        }
+
+        /// <summary>
+        /// On mouse down we set custom cursor if left button clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void picSource_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                DragImage(picSource.Image, true) ;
+            };
+        }
+
+        private void picSource_MouseUp(object sender, MouseEventArgs e)
+        {
+            //Reset Cursor to default
+            Cursor = Cursors.Default;
+
+            if (DragDropOK)
+            {
+                //Set dest image
+                picDestination.Image = picSource.Image;
+                DragDropOK = false;
+                picDestination.Refresh();
+            }
+        }
+
+        /// <summary>
+        /// Resizes Image keepping Aspect
+        /// </summary>
+        /// <param name="imgToResize"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        private static Image ResizeImage(System.Drawing.Image imgToResize, Size size)
+        {
+            // Get the image current width
+            int sourceWidth = imgToResize.Width;
+            // Get the image current height
+            int sourceHeight = imgToResize.Height;
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+            // Calculate width and height with new desired size
+            nPercentW = ((float)size.Width / (float)sourceWidth);
+            nPercentH = ((float)size.Height / (float)sourceHeight);
+            nPercent = Math.Min(nPercentW, nPercentH);
+            // New Width and Height
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+            Bitmap b = new Bitmap(destWidth, destHeight);
+            Graphics g = Graphics.FromImage((System.Drawing.Image)b);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            // Draw image with new width and height
+            g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+            g.Dispose();
+            return (Image)b;
+        }
+
+        /// <summary>
+        /// Only allow integers in textbox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtScaleImage_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+
+        private void picSource_MouseMove(object sender, MouseEventArgs e)
+        {
+           //rect 1 and 2 collide allow image to be dropped
+            var rect1 = new Rectangle(picDestination.Location, picDestination.Size);
+
+            var rect2 = new Rectangle(e.Location, picSource.Size);
+
+            //Set size differnt if custom cursor is used
+            if (ckbScaleImage.Checked)
+            {
+                rect2 = new Rectangle(e.Location, new Size(ImageWidth, ImageHeight)); 
+            }
+
+            if (rect1.IntersectsWith(rect2))
+            {
+                //code when collided
+                DragDropOK = true;
+                picDestination.Refresh();
+                lblDropImageOK.Visible = true;
+            }
+            else
+            {
+                DragDropOK=false;
+            }
+        }
+
+        private void picDestination_Paint(object sender, PaintEventArgs e)
+        {
+            if (DragDropOK)
+            {
+                //Create yelloyish border if ok to drop image
+                ControlPaint.DrawBorder(e.Graphics, picDestination.ClientRectangle,
+                  Color.GreenYellow, 4, ButtonBorderStyle.Solid,  // Left
+                  Color.GreenYellow, 4, ButtonBorderStyle.Solid,  // Top
+                  Color.GreenYellow, 4, ButtonBorderStyle.Solid,  // Right
+                  Color.GreenYellow, 4, ButtonBorderStyle.Solid); // Bottom
+            }
+            else
+            {
+                lblDropImageOK.Visible = false;
+
+                //Remove border after drop or any time drag and drop not permitted
+                ControlPaint.DrawBorder(e.Graphics, picDestination.ClientRectangle,
+                 Color.Transparent, 0, ButtonBorderStyle.None,  // Left
+                 Color.Transparent, 0, ButtonBorderStyle.None,  // Top
+                 Color.Transparent, 0, ButtonBorderStyle.None,  // Right
+                 Color.Transparent, 0, ButtonBorderStyle.None); // Bottom
+            }
+           
+        }
+
+        //Used For Refreshing picDestination only
+        private void button1_Click(object sender, EventArgs e)
+        {
+            picDestination.Image = null;
+        }
+
+    }
+}
